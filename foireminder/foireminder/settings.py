d99052f49504d8c59dfa3f531ddeb9e0f096aead
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-11-09 17:50:41 -0200

    Fix bad DEBUG setting

--- foireminder/foireminder/settings.py
+++ foireminder/foireminder/settings.py
@@ -3,7 +3,7 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = 'PRODUCTION' in os.environ
+DEBUG = not 'PRODUCTION' in os.environ
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
