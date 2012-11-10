4e974e83cfb9f7f8acf23931106dfe090136cb15
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-11-10 16:20:30 -0200

    Add view to change language

--- foireminder/foireminder/urls.py
+++ foireminder/foireminder/urls.py
@@ -6,10 +6,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'reminders.views.index', name='index'),
-    url(r'^new$', 'reminders.views.new', name='new'),
-    url(r'^request-made/(?P<pk>\d+)$', 'reminders.views.request_made', name='request_made'),
-
+    url(r'^$', 'foireminder.reminders.views.index', name='index'),
+    url(r'^new$', 'foireminder.reminders.views.new', name='new'),
+    url(r'^request-made/(?P<pk>\d+)$', 'foireminder.reminders.views.request_made',
+        name='request_made'),
+    (r'^language/', include('django.conf.urls.i18n')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
